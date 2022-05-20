ec058d266d433b1e5a8ddfcdd22db8908a4f6d63
Mike McDargh <mike@mcdargh.com>
2022-05-20 15:27:03 -0400

    update

--- xorfilewithstring.py
+++ xorfilewithstring.py
@@ -1,23 +1,21 @@
 import sys
 
-def repeating_key_xor(plaintext, key):
-    if len(key) == 0 or len(key) > len(plaintext):
-        raise "KEY LENGTH EXCEPTION!"
-  
-    ciphertext_bytes = bytearray()
-    key_bytes = bytearray(key, encoding='UTF-8')
+def xor(plaintext, key):
+    result = bytearray()
+    key = bytearray(key, encoding='UTF-8')
   
     # XOR every byte of the plaintext with the corresponding byte from the key  
     for i in range( len(plaintext) ):
-        k = key_bytes[i % len(key)]
+        k = key[i % len(key)]
         c = plaintext[i] ^ k
-        ciphertext_bytes.append(c)
+        result.append(c)
       
-    return ciphertext_bytes
+    return result
 
 # Read file as byte array
-plaintext = bytearray(open(sys.argv[1], 'rb').read())
-
+ciphertext = bytearray(open(sys.argv[1], 'rb').read())
+# convert
+plaintext = xor(ciphertext, sys.argv[3])
+# Open outputfile and write result
 with open(sys.argv[2], "wb") as output:
-    result = repeating_key_xor(plaintext, sys.argv[3])
-    output.write(result)
+    output.write(plaintext)
